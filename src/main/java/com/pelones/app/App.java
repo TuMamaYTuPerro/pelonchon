d11da18b09903e3a258cd02b07f16074ad57c939
mxPanda <does0jff@gmail.com>
2025-10-13 16:53:50 -0600

    - Cambios para compilr con el pom.xml a un jar - Se agregan mas excepciones a gitignore - Se elimina el codigo comentado alv

--- src/main/java/com/pelones/app/App.java
+++ src/main/java/com/pelones/app/App.java
@@ -24,49 +24,6 @@ public class App {
             System.out.println(dynamicArray.getAt(i));
         }
 
-        //System.out.println("Hola mundo");
-        //stringUtils.printNumbers("T1u0G8fa9");
-        //stringUtils.printNumbers("1234 123");
-        //dynamicArray.add("miperro");
-        //dynamicArray.add("Tugfa");
-        //dynamicArray.add("Viejocochino");
-        //dynamicArray.add("no");
-        //dynamicArray.add("cochi");
-        //dynamicArray.add("Viejo");
-        //dynamicArray.add("nel");
-
-        //System.out.println("Antes de borrar: ");
-        //for(int len = 0; len < dynamicArray.size(); len++) {
-            //System.out.println(dynamicArray.getAt(len));
-        //}
-
-        //dynamicArray.erase(0);
-        //dynamicArray.erase(5);
-        //System.out.println("Despues de borrar: ");
-        //for(int len = 0; len < dynamicArray.size(); len++) {
-            //System.out.println(dynamicArray.getAt(len));
-        //}
-
-            //System.out.println("Escribe algo: "); //toSearch
-            //toSearch = scanner.nextLine();
-            //index = dynamicArray.search(toSearch);
-
-            //if (index >= 0){
-            //    System.out.println("El objeto se encuentra en el indice: " + index);
-            //}
-
-        //try {
-            //stringUtils.printDummy("Hola");
-            //stringUtils.printDummy(null);
-            //stringUtils.printDummy("null");
-            //System.out.println("Ingresa un numero");
-            //number = stringUtils.numbersValue(scanner.nextLine());
-            //System.out.println(number);
-        //} catch(NullPointerException e) {
-            //System.out.println("Hubo un error: " + e.getMessage());
-        //} catch(NotANumberException ex) {
-            //System.out.println("No es un numero: " + ex.getMessage());
-        //}
         
         scanner.close();
     }
